LucaModric/PiliNara

feat(dynamics): 动态页面添加文本过滤功能
- 动态内容支持长按选择文本后加入过滤规则
- 自由复制界面同样支持文本过滤
- 统一评论区和动态页的菜单项顺序（插入到第四个位置）
- 在 Android 上减少被折叠到三点菜单的概率

lib/pages/dynamics/widgets/content_panel.dart
@@ -1,11 +1,16 @@
 // 内容
 import 'package:PiliPlus/common/widgets/custom_icon.dart';
+import 'package:PiliPlus/common/widgets/dialog/dialog.dart';
 import 'package:PiliPlus/common/widgets/flutter/text/text.dart' as custom_text;
 import 'package:PiliPlus/common/widgets/image_grid/image_grid_view.dart';
 import 'package:PiliPlus/models/dynamics/result.dart';
 import 'package:PiliPlus/pages/dynamics/widgets/rich_node_panel.dart';
 import 'package:PiliPlus/utils/page_utils.dart';
+import 'package:PiliPlus/utils/storage.dart';
+import 'package:PiliPlus/utils/storage_key.dart';
+import 'package:PiliPlus/utils/storage_pref.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
 
 Widget content(
@@ -108,11 +113,62 @@ Widget content(
 }
 
 Widget _contextMenuBuilder(EditableTextState state, String text) {
-  return AdaptiveTextSelectionToolbar.buttonItems(
-    buttonItems: state.contextMenuButtonItems
-      ..add(
-        ContextMenuButtonItem(label: '文本', onPressed: () => _onCopyText(text)),
+  final items = state.contextMenuButtonItems;
+  if (!state.textEditingValue.selection.isCollapsed) {
+    // 插入到第四个位置（索引3），即在"复制"、"全选"、"分享"等系统默认项之后
+    // 这样在 Android 上可以让"加入过滤"更优先显示，减少被折叠的概率
+    final insertIndex = items.length >= 3 ? 3 : items.length;
+    items.insert(
+      insertIndex,
+      ContextMenuButtonItem(
+        onPressed: () {
+          Navigator.of(state.context).pop();
+          final select = state.textEditingValue;
+          final escapedText = RegExp.escape(
+            select.selection.textInside(select.text),
+          );
+
+          showConfirmDialog(
+            context: state.context,
+            title: const Text('是否将以下内容加入动态过滤：'),
+            content: Text(
+              escapedText,
+              style: const TextStyle(
+                color: Colors.green,
+                fontWeight: .bold,
+              ),
+            ),
+            onConfirm: () {
+              final currentStored = Pref.banWordForDyn;
+              // 检查是否已存在（按行分割检查）
+              final existingKeywords = currentStored.isEmpty
+                  ? <String>[]
+                  : currentStored.split('\n');
+              if (existingKeywords.contains(escapedText)) {
+                SmartDialog.showToast('该关键词已在过滤列表中');
+                return;
+              }
+              final newStored = currentStored.isEmpty
+                  ? escapedText
+                  : '$currentStored\n$escapedText';
+              GStorage.setting.put(SettingBoxKey.banWordForDyn, newStored);
+              final newPattern = Pref.parseBanWordToRegex(newStored);
+              DynamicsDataModel.banWordForDyn =
+                  RegExp(newPattern, caseSensitive: true);
+              DynamicsDataModel.enableFilter = true;
+              SmartDialog.showToast('已保存');
+            },
+          );
+        },
+        label: '加入过滤',
       ),
+    );
+  }
+  items.add(
+    ContextMenuButtonItem(label: '文本', onPressed: () => _onCopyText(text)),
+  );
+  return AdaptiveTextSelectionToolbar.buttonItems(
+    buttonItems: items,
     anchors: state.contextMenuAnchors,
   );
 }
@@ -126,6 +182,7 @@ void _onCopyText(String text) {
         child: SelectableText(
           text,
           style: const TextStyle(fontSize: 15, height: 1.7),
+          contextMenuBuilder: (_, state) => _contextMenuBuilder(state, text),
         ),
       ),
     ),

ekmope/PiliMax

lib/pages/dynamics/widgets/content_panel.dart
@@ -1,11 +1,16 @@
 // 内容
 import 'package:PiliMax/common/widgets/custom_icon.dart';
+import 'package:PiliMax/common/widgets/dialog/dialog.dart';
 import 'package:PiliMax/common/widgets/flutter/text/text.dart' as custom_text;
 import 'package:PiliMax/common/widgets/image_grid/image_grid_view.dart';
 import 'package:PiliMax/models/dynamics/result.dart';
 import 'package:PiliMax/pages/dynamics/widgets/rich_node_panel.dart';
 import 'package:PiliMax/utils/page_utils.dart';
+import 'package:PiliMax/utils/storage.dart';
+import 'package:PiliMax/utils/storage_key.dart';
+import 'package:PiliMax/utils/storage_pref.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
 
 Widget content(
@@ -108,8 +113,56 @@ Widget content(
 }
 
 Widget _contextMenuBuilder(EditableTextState state, String text) {
+  final items = state.contextMenuButtonItems;
+  if (!state.textEditingValue.selection.isCollapsed) {
+    final insertIndex = items.length >= 3 ? 3 : items.length;
+    items.insert(
+      insertIndex,
+      ContextMenuButtonItem(
+        onPressed: () {
+          Navigator.of(state.context).pop();
+          final select = state.textEditingValue;
+          final escapedText = RegExp.escape(
+            select.selection.textInside(select.text),
+          );
+
+          showConfirmDialog(
+            context: state.context,
+            title: const Text('是否将以下内容加入动态过滤：'),
+            content: Text(
+              escapedText,
+              style: const TextStyle(
+                color: Colors.green,
+                fontWeight: .bold,
+              ),
+            ),
+            onConfirm: () {
+              final currentStored = Pref.banWordForDyn;
+              final existingKeywords = currentStored.isEmpty
+                  ? <String>[]
+                  : currentStored.split('\n');
+              if (existingKeywords.contains(escapedText)) {
+                SmartDialog.showToast('该关键词已在过滤列表中');
+                return;
+              }
+              final newStored = currentStored.isEmpty
+                  ? escapedText
+                  : '$currentStored\n$escapedText';
+              GStorage.setting.put(SettingBoxKey.banWordForDyn, newStored);
+              final newPattern = Pref.parseBanWordToRegex(newStored);
+              DynamicsDataModel.banWordForDyn =
+                  RegExp(newPattern, caseSensitive: true);
+              DynamicsDataModel.enableFilter = true;
+              SmartDialog.showToast('已保存');
+            },
+          );
+        },
+        label: '加入过滤',
+      ),
+    );
+  }
   return AdaptiveTextSelectionToolbar.buttonItems(
-    buttonItems: state.contextMenuButtonItems
+    buttonItems: items
       ..add(
         ContextMenuButtonItem(label: '文本', onPressed: () => _onCopyText(text)),
       ),
@@ -126,6 +179,7 @@ void _onCopyText(String text) {
         child: SelectableText(
           text,
           style: const TextStyle(fontSize: 15, height: 1.7),
+          contextMenuBuilder: (_, state) => _contextMenuBuilder(state, text),
         ),
       ),
     ),